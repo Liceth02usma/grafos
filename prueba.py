17bfeba106188d318bea9cbcfd7355374dc7af97
Liceth02usma <90731962+Liceth02usma@users.noreply.github.com>
2023-06-23 10:29:41 -0500

    Commit 2

--- prueba.py
+++
@@ -1,8 +0,0 @@
-from clases.grafo import *
-import random
-from interfaz import *
-import json
-from tkinter import simpledialog, messagebox 
-from tkinter import *
-from helpers.Keyboard import *
-
